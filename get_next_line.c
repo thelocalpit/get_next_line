60b57655131b655f2cfb7771a09bee20090c9367
Pietro Falaschi <pfalasch@e3r9p8.42roma.it>
2023-02-07 18:58:38 +0100

    aggiornamento serale

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,70 @@
 /*   By: pfalasch <pfalasch@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 10:45:43 by pfalasch          #+#    #+#             */
-/*   Updated: 2023/02/07 10:45:59 by pfalasch         ###   ########.fr       */
+/*   Updated: 2023/02/07 18:57:18 by pfalasch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+char	*get_line(int fd, char *line)
 {
-	
+	char	*buf;
+	size_t	i;
+
+	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (NULL);
+	i = 1;
+	while (!(ft_strchr(line, '\n')) && i > 0)
+	{
+		i = read(fd, buf, BUFFER_SIZE);
+		if (i == -1)
+		{
+			free(buf);
+			return (NULL);
+		}
+		buf[i] = '\0';
+		line = ft_strjoin(line, buf);
+	}
+	free(buf);
+	return (line);
+}
+
+char	*get_new_line(char *line)
+{
+	size_t	len;
+	char	*s2;
+
+	len = 0;
+	while (line[len] != '\n' || line[len])
+		len++;
+	s2 = (char *)malloc(sizeof(char) * (len + 1));
+	if (!s2)
+		return (0);
+	ft_memcpy(s2, line, (len + 1));
+	return (s2);
+}
+
+char	*clean_new_line(char *line)
+{
+	size_t	i;
+
+	i = 0;
+	while (line[i] != '\n' ||)
+		i++;
+	return (line + i + 1);
+}
+
+int	*get_next_line(int fd)
+{
+	static char	*line;
+	char		*string;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	line = get_line(line);
+	string = get_new_line(line);
+	line = clean_new_line(line);
+	return (string);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,22 +6,23 @@
 /*   By: pfalasch <pfalasch@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 10:46:46 by pfalasch          #+#    #+#             */
-/*   Updated: 2023/02/07 12:11:48 by pfalasch         ###   ########.fr       */
+/*   Updated: 2023/02/07 18:56:01 by pfalasch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int ft_strlen(const char *str)
+int	ft_strlen(const char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
 		i++;
 	return (i);
 }
-char *ft_strchr(const char *s, int c)
+
+char	*ft_strchr(const char *s, int c)
 {
 	while (*s != '\0' && c != *s)
 		s++;
@@ -30,15 +31,20 @@ char *ft_strchr(const char *s, int c)
 	return (0);
 }
 
-char *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char *new_str;
-	int i;
-	int j;
+	char	*new_str;
+	int		i;
+	int		j;
 
-	if (!s1 || !s2)
+	if (!s2)
 		return (NULL);
 	new_str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!s1)
+	{
+		s1 = (char *)malloc(sizeof(char) * 1);
+		s1[0] = '\0';
+	}
 	if (!new_str)
 		return (NULL);
 	i = 0;
@@ -49,36 +55,7 @@ char *ft_strjoin(char const *s1, char const *s2)
 	}
 	j = 0;
 	while (s2[j] != '\0')
-	{
-		new_str[i] = s2[j];
-		i++;
-		j++;
-	}
+		new_str[i++] = s2[j++];
 	new_str[i] = '\0';
 	return (new_str);
 }
-
-char	*get_line(int fd, char *line)
-{
-	char	*buf;
-	size_t 	i;
-
-	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1);
-	if (!buf)
-		return (NULL);
-	i = 1;
-	while (!(ft_strchr(line, '\n')) && i > 0)
-	{
-		i = read(fd, buf, BUFFER_SIZE);
-		if (i == -1)
-		{
-			free(buf);
-			return (NULL);
-		}
-		line = ft_strjoin(line, buf);
-		free(buf);
-	}
-	free(buf);
-	return (line);
-}
-	
