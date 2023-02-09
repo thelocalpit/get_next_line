225a72a23f95de70a0a1bf2aaafa1c170ebd537c
Pietro Falaschi <pfalasch@e3r7p5.42roma.it>
2023-02-09 15:04:58 +0100

    update

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pfalasch <pfalasch@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pfalasch <pfalasch@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 10:45:43 by pfalasch          #+#    #+#             */
-/*   Updated: 2023/02/08 16:42:44 by pfalasch         ###   ########.fr       */
+/*   Updated: 2023/02/09 14:50:51 by pfalasch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,17 +38,27 @@ char	*get_line(int fd, char *line)
 
 char	*get_new_line(char *line)
 {
-	size_t	len;
-	char	*s2;
+	size_t	i;
+	char	*next_str;
 
-	len = 0;
-	while (line[len] != '\n' || line[len])
-		len++;
-	s2 = (char *)malloc(sizeof(char) * (len + 1));
-	if (!s2)
+	i = 0;
+	while (line[i] != '\n' || line[i])
+		i++;
+	next_str = (char *)malloc(sizeof(char) * (i + 1));
+	if (!next_str)
 		return (0);
-	ft_memcpy(s2, line, (len + 1));
-	return (s2);
+	i = 0;
+	while (line[i] && line[i] != '/n')
+	{
+		next_str[i] = line[i];
+		i++;
+	}
+	if (line[i] == '/n')
+	{
+		next_str[i++] = '/n';
+	}
+	next_str[i] = 0;
+	return (next_str);
 }
 
 char	*clean_new_line(char *line)
@@ -75,14 +85,14 @@ char	*clean_new_line(char *line)
 char	*get_next_line(int fd)
 {
 	static char	*line;
-	char		*string;
+	char		*string_out;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	line = get_line(fd, line);
 	if (!line)
 		return (NULL);
-	string = get_new_line(line);
+	string_out = get_new_line(line);
 	line = clean_new_line(line);
-	return (string);
+	return (string_out);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,18 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pfalasch <pfalasch@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pfalasch <pfalasch@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 10:46:46 by pfalasch          #+#    #+#             */
-/*   Updated: 2023/02/08 20:43:16 by pfalasch         ###   ########.fr       */
+/*   Updated: 2023/02/09 14:58:45 by pfalasch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	ft_strlen(const char *str)
+size_t	ft_strlen(const char *str)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
 	while (str[i] != '\0')
@@ -24,6 +24,8 @@ int	ft_strlen(const char *str)
 
 char	*ft_strchr(const char *s, int c)
 {
+	if (!s)
+		return (NULL);
 	while (*s != '\0' && c != *s)
 		s++;
 	if (*s == c)
@@ -34,26 +36,24 @@ char	*ft_strchr(const char *s, int c)
 char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*new_str;
-	int		i;
-	int		j;
+	size_t	i;
+	size_t	j;
 
 	if (!s2)
 		return (NULL);
-	new_str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	if (!s1)
 	{
 		s1 = (char *)malloc(sizeof(char) * 1);
 		s1[0] = '\0';
 	}
+	new_str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	if (!new_str)
 		return (NULL);
 	i = 0;
-	while (s1[i])
-	{
-		new_str[i] = s1[i];
-		i++,
-	}
 	j = 0;
+	while (s1[i])
+		new_str[j++] = s1[i++];
+	i = 0;
 	while (s2[j])
 		new_str[i++] = s2[j++];
 	new_str[i] = '\0';
